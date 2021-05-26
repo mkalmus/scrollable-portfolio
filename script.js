a69eb881615736c0db2a40f358dbc7bfcd842694
Michael Kalmus <>
2021-05-26 08:17:05 +0000

    i hate fixed navbars

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.addEventListener('click', function (event) {
+// 	if (!event.target.matches('.nav-link')) return;
+//   // document.getElementById('project-page').style.paddingTop += 20 + "vh";
+//   console.log(document.getElementById('project-page').style.top)
+//   document.getElementById('project-page').style.top += 20 + "vh"
+// }, false);
+
+// window.addEventListener("scroll", function(){
+//   var header = document.querySelector("header")
+//   //if we're scrolled at all, add sticky class
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// })
