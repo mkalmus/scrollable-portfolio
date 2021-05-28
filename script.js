204ff8ee4ce9f6f3bfd7ed926c23f762e608cdbf
Michael Kalmus <>
2021-05-28 05:35:22 +0000

    tons of awesome stuff

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// document.addEventListener('click', function (event) {
-// 	if (!event.target.matches('.nav-link')) return;
-//   // document.getElementById('project-page').style.paddingTop += 20 + "vh";
-//   console.log(document.getElementById('project-page').style.top)
-//   document.getElementById('project-page').style.top += 20 + "vh"
-// }, false);
-
 // window.addEventListener("scroll", function(){
 //   var header = document.querySelector("header")
 //   //if we're scrolled at all, add sticky class
 //   header.classList.toggle("sticky", window.scrollY > 0);
 // })
+
+var nav_links = document.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < nav_links.length; i++) {
+  nav_links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+
+
+
+//ADD LOGIC to also change active nav based on current scroll location
